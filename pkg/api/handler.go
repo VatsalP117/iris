6e7c51fe831c63f91df059b015eb04df4becd6eb
Vatsal Patel <vatsal4011@gmail.com>
2026-02-21 20:32:00 +0530

    cors config fix

--- pkg/api/handler.go
+++ pkg/api/handler.go
@@ -18,9 +18,16 @@ func NewHandler(repo core.EventRepository) *Handler {
 }
 
 func setCORSHeaders(w http.ResponseWriter, r *http.Request) bool {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	origin := r.Header.Get("Origin")
+	if origin == "" {
+		origin = "*"
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", origin)
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
 		return true
