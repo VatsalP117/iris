a817c0ace60147244c4f4bc63bcef4ba0c8db0e5
Vatsal Patel <vatsal4011@gmail.com>
2026-02-22 18:11:58 +0530

    basic refactor to use constants

---
+++ web/src/constants.ts
@@ -0,0 +1,17 @@
+export const API_ENDPOINTS = {
+    SINGLE_EVENT: "api/event",
+    BATCH_EVENTS: "api/events",
+}
+
+export const API_REQUEST_CONSTANTS = {
+    HEADERS: {
+        CONTENT_TYPE: "Content-Type",
+    },
+    CONTENT_TYPES: {
+        JSON: "application/json",
+    },
+    METHODS: {
+        POST: "POST",
+        GET: "GET",
+    }
+}
--- web/src/transport.ts
+++ web/src/transport.ts
@@ -1,21 +1,22 @@
 import { IrisConfig, EventPayload } from "./config";
+import { API_ENDPOINTS, API_REQUEST_CONSTANTS } from "./constants";
 
 export class Transport {
   constructor(private config: IrisConfig) { }
 
   send(payload: EventPayload) {
-    const url = `${this.config.host}/api/event`;
+    const url = `${this.config.host}/${API_ENDPOINTS.SINGLE_EVENT}`;
     const body = JSON.stringify(payload);
 
     if (navigator.sendBeacon) {
-      const blob = new Blob([body], { type: "application/json" });
+      const blob = new Blob([body], { type: API_REQUEST_CONSTANTS.CONTENT_TYPES.JSON });
       navigator.sendBeacon(url, blob);
     } else {
       fetch(url, {
-        method: "POST",
+        method: API_REQUEST_CONSTANTS.METHODS.POST,
         body: body,
         keepalive: true,
-        headers: { "Content-Type": "application/json" },
+        headers: { [API_REQUEST_CONSTANTS.HEADERS.CONTENT_TYPE]: API_REQUEST_CONSTANTS.CONTENT_TYPES.JSON },
       }).catch((err) => {
         if (this.config.debug) console.error("Iris: Failed to send", err);
       });
